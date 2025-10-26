b6c56e3a3e20c80755a68facfbfd89ef901ef613
novusaevum <wmh.dirc@gmail.com>
2025-10-27 07:54:21 +0800

    Finalize CRUD templates and navigation

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -24,8 +24,12 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import path, include
 
+from django.urls import path, include
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/books/', permanent=False)),  # Redirect root "/" to "/books/"
     path('', include('books.urls')),
 ]
 
